a160270ceb9cb339d508a6aa1b9e4f84172e4f41
amirhossein-gandomkar <137129614+amirhossein-gandomkar@users.noreply.github.com>
2026-02-27 15:19:11 +0330

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import MangaEditor from './components/MangaEditor';
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-zinc-950">
+      <MangaEditor />
+    </div>
+  );
+}
